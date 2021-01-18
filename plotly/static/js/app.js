0a4ced569bb7309980bc3551024b62717d4e0d74
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-18 03:29:54 -0500

    create barchart onchange

--- plotly/static/js/app.js
+++ plotly/static/js/app.js
@@ -5,111 +5,67 @@ d3.json("data/samples.json").then((samplesData) => {
     samplesData.names.forEach((sample) => {
         dropdown.append("option").text(sample).property("value", sample);
     });
+    var sampleID = dropdown.property("value");
+    console.log(sampleID);
     console.log(samplesData);
-    createBarChart(samplesData);
-    // // initialize webpage with data from first sampleID
-    // var initSample = samplesData.names[0];
-    // var initData = samplesData.samples[0];
-
-    // // get sample_values first 10
-    // var revValues = initData.sample_values.slice(0,10).reverse();
-    // var otuIDs = initData.otu_ids.slice(0,10).reverse();
-    // var strRevIDs = otuIDs.map(row => "OTU " + row.toString());
-    // var revLabels = initData.otu_labels.slice(0,10).reverse();
-
-    // // horizontal bar chart
-    // var data0 = [{
-    //     x: revValues,
-    //     y: strRevIDs,
-    //     text: revLabels,
-    //     name: initSample,
-    //     type: "bar",
-    //     orientation: "h"
-    // }];
-
-    // var layout0 = {
-    //     barmode: "group"
-    // };
-
-    // Plotly.newPlot("bar", data0, layout0);
-
-    // create bubble chart
-    createBubbleChart(samplesData);
-
-    // var otuIDstr = initData.otu_ids.map(String);
-    // console.log(otuIDstr);
-
-    // var data1 = [{
-    //     x: initData.otu_ids,
-    //     y: initData.sample_values,
-    //     text: initData.otu_labels,
-    //     mode: "markers",
-    //     marker: {
-    //         color: otuIDstr,
-    //         size: initData.sample_values
-    //     }
-    // }];
-
-    // var layout1 = {
-    //     xaxis: { title: "OTU ID"},
-    //     showlegend: false
-    // };
-
-    // Plotly.newPlot("bubble",data1, layout1);
-    displayMetadata(samplesData);
-    // // display metadata
-    // // get reference to panel body
-    // var dataPanel = d3.select("#sample-metadata");
-    // // clear data
-    // dataPanel.html("");
-    // console.log(samplesData.metadata[0]);
-    // var metadata = samplesData.metadata[0];
-
-    // Object.entries(metadata).forEach(([key, value]) => {
-    //     //var panelBody = dataPanel.append("div").attr("class","panel-body").text(`${key}: ${value}`);
-    //     var panelBody = dataPanel.append("p").text(`${key}: ${value}`);
-    //     console.log(`${key}: ${value}`);
-    // });
-
+    createBarChart(sampleID);
+    createBubbleChart(samplesID);
+    //displayMetadata(samplesData);
 });
 
 function optionChanged(sampleID) {
     // get data based on the sampleID
     console.log(sampleID);
+    createBarChart(sampleID);
 }
 
-function createBarChart(selectedData) {
-    // initialize webpage with data from first sampleID
-    var initSample = selectedData.names[0];
-    var initData = selectedData.samples[0];
-    // get sample_values first 10
-    var revValues = initData.sample_values.slice(0,10).reverse();
-    var otuIDs = initData.otu_ids.slice(0,10).reverse();
-    var strRevIDs = otuIDs.map(row => "OTU " + row.toString());
-    var revLabels = initData.otu_labels.slice(0,10).reverse();
+function createBarChart(selectedID) {
+    console.log(selectedID.toString());
 
-    // horizontal bar chart
-    var data0 = [{
-        x: revValues,
-        y: strRevIDs,
-        text: revLabels,
-        name: initSample,
-        type: "bar",
-        orientation: "h"
-    }];
+    d3.json("data/samples.json").then((data) => {
+        data.samples.forEach((sample) => {
+            if (sample.id == selectedID.toString()) {
+                console.log("equal");
+                selectedData = sample;
+                // initialize webpage with data from first sampleID
+                var initSample = selectedData.id;
+                var initData = selectedData;
+                console.log(initData);
 
-    var layout0 = {
-        barmode: "group"
-    };
+                // get sample_values first 10
+                var revValues = initData.sample_values.slice(0,10).reverse();
+                var otuIDs = initData.otu_ids.slice(0,10).reverse();
+                var strRevIDs = otuIDs.map(row => "OTU " + row.toString());
+                var revLabels = initData.otu_labels.slice(0,10).reverse();
 
-    Plotly.newPlot("bar", data0, layout0);
+                // horizontal bar chart
+                var data0 = [{
+                    x: revValues,
+                    y: strRevIDs,
+                    text: revLabels,
+                    name: initSample,
+                    type: "bar",
+                    orientation: "h"
+                }];
+
+                var layout0 = {
+                    barmode: "group"
+                };
+
+                Plotly.newPlot("bar", data0, layout0);
+
+            }
+        
+        })
+    })
 
 };
 
 function createBubbleChart(selectedData) {
     // initialize webpage with data from first sampleID
-    var initSample = selectedData.names[0];
-    var initData = selectedData.samples[0];
+    var sampleIndex = 0;
+    var initSample = selectedData.names[sampleIndex];
+    var initData = selectedData.samples[sampleIndex];
     var otuIDstr = initData.otu_ids.map(String);
     console.log(otuIDstr);
 
@@ -134,13 +90,14 @@ function createBubbleChart(selectedData) {
 };
 
 function displayMetadata(selectedData) {
+    var sampleIndex = 0;
     // display metadata
     // get reference to panel body
     var dataPanel = d3.select("#sample-metadata");
     // clear data
     dataPanel.html("");
-    console.log(selectedData.metadata[0]);
-    var metadata = selectedData.metadata[0];
+    console.log(selectedData.metadata[sampleIndex]);
+    var metadata = selectedData.metadata[sampleIndex];
 
     Object.entries(metadata).forEach(([key, value]) => {
         //var panelBody = dataPanel.append("div").attr("class","panel-body").text(`${key}: ${value}`);
