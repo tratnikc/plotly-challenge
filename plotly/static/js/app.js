88f802105f87f2bb14e6f317fcb99e00764cb13c
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-17 23:01:45 -0500

    Update app.js

--- plotly/static/js/app.js
+++ plotly/static/js/app.js
@@ -4,32 +4,17 @@ var dropdown = d3.select("#selDataset");
 d3.json("samples.json").then((samplesData) => {
     samplesData.names.forEach((sample) => {
         dropdown.append("option").text(sample).property("value", sample);
-
     });
 
     // initialize webpage with data from first sampleID
     var initSample = samplesData.names[0];
     var initData = samplesData.samples[0];
-    var values = initData.sample_values;
-    var ids = initData.otu_ids;
-    var labels = initData.otu_labels;
-
-    console.log(values);
 
     // get sample_values first 10
-    var sampleValues = initData.sample_values.slice(0,10);
-    var revValues = sampleValues.reverse();
-    var otu_ids = initData.otu_ids.slice(0,10);
-    var revIDs = otu_ids.reverse();
-    var strRevIDs = revIDs.map(row => "OTU " + row.toString());
-    var otu_labels = initData.otu_labels.slice(0,10);
-    var revLabels = otu_labels.reverse();
-
-    console.log(revValues);
-    console.log(revIDs);
-    console.log(strRevIDs);
-    console.log(otu_labels);
-    console.log(initSample);
+    var revValues = initData.sample_values.slice(0,10).reverse();
+    var otuIDs = initData.otu_ids.slice(0,10).reverse();
+    var strRevIDs = otuIDs.map(row => "OTU " + row.toString());
+    var revLabels = initData.otu_labels.slice(0,10).reverse();
 
     // horizontal bar chart
     var data0 = [{
@@ -46,7 +31,6 @@ d3.json("samples.json").then((samplesData) => {
     };
 
     Plotly.newPlot("bar", data0, layout0);
-
 });
 
 function optionChanged(sampleID) {
