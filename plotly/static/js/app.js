2f9df6d76250777efdb76b12876e74b33923e767
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-17 19:19:47 -0500

    optionChanged

--- plotly/static/js/app.js
+++ plotly/static/js/app.js
@@ -5,11 +5,24 @@ d3.json("../data/samples.json").then((samplesData) => {
     samplesData.names.forEach((sample) => {
         dropdown.append("option").text(sample).property("value", sample);
 
-    })
+    });
+
+    // initialize webpage with data from first sampleID
+    var initSample = samplesData.names[0];
+    // horizontal bar chart
+    // var trace0 = {
+        
+
+    // }
+    console.log(initSample);
     console.log(samplesData);
     console.log(samplesData.metadata);
     console.log(samplesData.names);
-    console.log(samplesData.samples[0].id);
+    console.log(samplesData.samples);
 
 });
 
+function optionChanged(sampleID) {
+    // get data based on the sampleID
+    console.log(sampleID);
+}
